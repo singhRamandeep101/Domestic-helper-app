eb6ca893e0f65d9b759e0877641b12f7dcf5daaf
JackWong0128 <plljknbh@gmail.com>
2025-04-09 19:32:14 +0800

    saveTimeselect

--- FypProject/app/src/main/java/com/project/fypproject/activities/InterviewTimeInvitedActivity.java
+++ FypProject/app/src/main/java/com/project/fypproject/activities/InterviewTimeInvitedActivity.java
@@ -1,6 +1,9 @@
 package com.project.fypproject.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +12,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.project.fypproject.R;
+import com.project.fypproject.activities.employer.JobListActivity;
 
 public class InterviewTimeInvitedActivity extends AppCompatActivity {
+    Button btnGoJobList,btnGoApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_interview_time_invited);
+
+        btnGoJobList = findViewById(R.id.btnGoToJobList);
+        btnGoApp = findViewById(R.id.btnGoToAppointment);
+
+        btnGoJobList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InterviewTimeInvitedActivity.this, JobListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
