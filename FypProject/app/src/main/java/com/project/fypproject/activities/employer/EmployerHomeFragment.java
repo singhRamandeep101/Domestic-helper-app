cee5f5188f11aec268404ecb4234db9ae20f97d7
JackWong0128 <plljknbh@gmail.com>
2025-04-13 02:49:30 +0800

    Update EmployerHomeFragment.java

--- FypProject/app/src/main/java/com/project/fypproject/activities/employer/EmployerHomeFragment.java
+++ FypProject/app/src/main/java/com/project/fypproject/activities/employer/EmployerHomeFragment.java
@@ -98,7 +98,7 @@ public class EmployerHomeFragment extends Fragment {
         llJobPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                changeActivity(BookRecordActivity.class);
+                changeActivity(BookingRequestActivity.class);
             }
         });
 
